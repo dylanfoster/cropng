c9602b9e5867efcb60807fd25ae18d5fe90ccb08
Dylan Foster <dylan947@gmail.com>
2015-07-02 23:02:15 -0700

    feat(crop): adds crop method

--- src/cropng.js
+++ src/cropng.js
@@ -1,11 +1,107 @@
 "use strict";
 
 import { ok as Assert } from "assert";
+import fs from "fs";
+import path from "path";
+
+import existsSync from "exists-sync";
+import fileType from "file-type";
+import { PNG } from "node-png";
+import readChunk from "read-chunk";
 
 class Cropng {
   constructor(image) {
     Assert(image, "Missing required image");
     Assert(Buffer.isBuffer(image) || typeof image === "string", "Image must be a string or buffer");
+
+    let mime;
+
+    if (!Buffer.isBuffer(image)) {
+      Assert(existsSync(path.resolve(image)) === true, `Image could not be found: ${image}`);
+      mime = this._getMimeFromPath(image);
+    } else {
+      mime = this._getMimeFromBuffer(image);
+    }
+
+    Assert(mime === "image/png", `Invalid MIME type: ${mime}`);
+
+    this.image = image;
+  }
+
+  /**
+   * crop
+   *
+   * @param { x: x
+   * @param y: y
+   * @param height: height
+   * @param width: width }
+   * @param callback
+   * @return {undefined}
+   */
+  crop({ x: x, y: y, height: height, width: width }, callback) {
+    let png = new PNG();
+    let parsable = this._bufferizeImage();
+
+    png.parse(parsable, (err, parsed) => {
+      if (err) { return callback(err); }
+
+      this.bitmap = {
+        data: new Buffer(parsed.data),
+        height: parsed.height,
+        width: parsed.width
+      };
+
+      x = Math.round(x);
+      y = Math.round(y);
+      height = Math.round(height);
+      width = Math.round(width);
+
+      let bitmap = new Buffer(this.bitmap.data.length);
+      let offset = 0;
+
+      this.scan(x, y, height, width, function (x, y, idx) {
+        let data = this.bitmap.data.readUInt32BE(idx, true);
+        bitmap.writeUInt32BE(data, offset, true);
+        offset += 4;
+      });
+
+      let image = {};
+      image.data = new Buffer(bitmap);
+      image.height = height;
+      image.width = width;
+      callback(null, image);
+    });
+  }
+
+  _bufferizeImage() {
+    if (!Buffer.isBuffer(this.image)) {
+      return fs.readFileSync(this.image);
+    }
+    return this.image;
+  }
+
+  _getMimeFromPath(filePath) {
+    let buffer = readChunk.sync(filePath, 0, 262);
+    if (fileType(buffer)) { return fileType(buffer).mime; }
+    return null;
+  }
+
+  _getMimeFromBuffer(buffer) {
+    if (fileType(buffer)) { return fileType(buffer).mime; }
+    return null;
+  }
+
+  scan(x, y, h, w, iter) {
+    [x, y, h, w].forEach(m => {
+      m = Math.round(m);
+    });
+
+    for (let _y = y; _y < (y + h); _y++) {
+      for (let _x = x; _x < (x + w); _x++) {
+        let idx = (this.bitmap.width * _y + _x) << 2;
+        iter.call(this, _x, _y, idx);
+      }
+    }
   }
 }
 
--- test/cropng_tests.js
+++ test/cropng_tests.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import fs from "fs";
+import path from "path";
+
 import { expect } from "chai";
 
 import PNG from "../src/cropng";
@@ -15,5 +18,44 @@ describe("Croppng", function () {
     expect(function () {
       new PNG({});
     }).to.throw("Image must be a string or buffer");
+  });
+
+  it("returns an error if image is not found", function () {
+    let _path = "/foo/bar/baz.png";
+
+    expect(function () {
+      new PNG(_path);
+    }).to.throw("Image could not be found: /foo/bar/baz");
+  });
+
+  it("returns an error if image file is not image/png", function () {
+    let image = path.resolve(__dirname, "fixtures/mario.jpeg");
+
+    expect(function () {
+      new PNG(image);
+    }).to.throw("Invalid MIME type: image/jpeg");
+  });
+
+  it("returns an error if image buffer is not image/png", function () {
+    let imagePath = path.resolve(__dirname, "fixtures/mario.jpeg");
+    let buff = fs.readFileSync(imagePath);
+
+    expect(function () {
+      new PNG(buff);
+    }).to.throw("Invalid MIME type: image/jpeg");
+  });
+
+  describe("#crop", function () {
+    it("crops an image from a path", function (done) {
+      let image = path.resolve(__dirname, "fixtures/mario.png");
+
+      let png = new PNG(image);
+      png.crop({ x: 0, y: 0, height: 50, width: 50 }, function (err, cropped) {
+        expect(err).to.eql(null);
+        expect(cropped.height).to.eql(50);
+        expect(cropped.width).to.eql(50);
+        done();
+      });
+    });
   });
 });
